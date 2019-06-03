49bef9fb2931a8ec8d204b14c5cf6e2f752f5669
Patrick Desmarais <patrick.desm@gmail.com>
2019-06-03 14:39:57 -0400

    Add comments and JSDoc to ContextualError.

--- src/utilities/contextualError.js
+++ src/utilities/contextualError.js
@@ -1,4 +1,9 @@
 class ContextualError {
+    /**
+     * @param {string} type 
+     * @param {string} message 
+     * @param {Error|ContextualError} nativeOrParentError 
+     */
     constructor(type, message, nativeOrParentError) {
         this.type = type;
         this.message = message;
@@ -6,12 +11,16 @@ class ContextualError {
         this.stack = null;
     }
 
+    /**
+     * @returns {string}
+     */
     toString() {
         const contextString = this.context.stack || this.context.toString();
-            // `this.context.stack` fetches the javascript Error's property.
+            // `this.context.stack` fetches the javascript Error's call stack.
         return `[${this.type}] ${this.message} ${contextString}`;
     }
 }
+
 ContextualError.ERROR_TYPES = {
     databaseError: "DATABASE_ERROR",
     invalidInput: "INVALID_INPUT",
