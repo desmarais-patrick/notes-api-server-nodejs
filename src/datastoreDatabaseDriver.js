1f06ebcb206d79962c15ffb1b8209845161cf60f
Patrick Desmarais <patrick.desm@gmail.com>
2019-06-03 14:40:17 -0400

    Add JSDoc to DatastoreDatabaseDriver.

--- src/datastoreDatabaseDriver.js
+++ src/datastoreDatabaseDriver.js
@@ -1,12 +1,22 @@
 const NOTE_KIND = "Note";
 
 class DatastoreDatabaseDriver {
+    /**
+     * @param {object} options 
+     * @param {function} options.ContextualErrorClass
+     * @param {function} options.DatastoreClass
+     */
     constructor(options) {
         this.ContextualErrorClass = options.ContextualErrorClass;
 
         this.datastore = new options.DatastoreClass();
     }
 
+    /**
+     * Retrieves notes from the database.
+     * 
+     * @param {DatastoreDatabaseDriver~getNotesCallback} callback 
+     */
     getNotes(callback) {
         const query = this.datastore.createQuery([NOTE_KIND]).limit(10);
         this.datastore.runQuery(query, (err, entities) => {
@@ -22,7 +32,16 @@ class DatastoreDatabaseDriver {
             callback(null, entities.map((e) => this.parseNote(e)));
         });
     }
+    /**
+     * @callback DatastoreDatabaseDriver~getNotesCallback
+     * @param {Error|ContextualError} err
+     * @param {object[]} parsedNotes
+     */
 
+    /**
+     * @param {object} datastoreEntity 
+     * @returns {object} Parsed note.
+     */
     parseNote(datastoreEntity) {
         const meta = datastoreEntity[this.datastore.KEY];
         const id = meta.id;
