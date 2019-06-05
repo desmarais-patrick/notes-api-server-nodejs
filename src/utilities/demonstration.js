56fa1d9c695fc56964b4a7e7a80ef1e144ca8bb1
Patrick Desmarais <patrick.desm@gmail.com>
2019-06-05 10:47:44 -0400

    Add a few notes about demonstration
    Add notes to put branch on pause, to come back later.

--- src/utilities/demonstration.js
+++ src/utilities/demonstration.js
@@ -1,17 +1,29 @@
 // DefaultLogger
+// +console logging
+// +file logging
+
+// System Statistics
+// +server started
+// +per request performance
+// +storage size
 
 class Demonstration {
     // Receive logger instance in constructor.
-    // Receives database driver for resetting records.
+    // Receive database driver for resetting records.
     // Receive settings for thresholds.
+    // (?) Store demonstration settings in a demo-config.js file.
 
-    // Sets interval to check for logs and reset database.
-    // Resets logs when done.
+    // Set interval to check for logs and reset database.
+    // +time before next reset
+    // +time since last reset
+
+    // Reset logs when done.
 }
 
 class DemonstrationLogger {
     // Register a log watcher.
-    // Set a maximum log
+    // Set a maximum limit on number of logs received
+    // (!) Limit should not be below demonstration thresholds.
 
     // Log a request and its type.
     // Log a "write" request size.
@@ -21,6 +33,13 @@ class DemonstrationLogger {
 }
 
 // Datastore +delete all records.
+// (?) Keep unmodified records. Diff!
 // Datastore +import backup.
+// (?) Use demo (and test) fixtures in code instead of backup.
+// Add cool quotes, demo-related and software-related notes when resetting.
+
+// `force-demo-data-reset/` endpoint.
+
+// Create a 429 Limit reached response before quotas are reached on server.
 
 module.exports = Demonstration;
