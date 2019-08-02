3a0fc4a444b73c77d0e83ac58fa5e0d053e9fb0f
Patrick Desmarais <patrick.desm@gmail.com>
2019-08-02 09:34:07 -0400

    Update database translation.
     - Fix working with numeric identifiers instead of names.
     - Fix excluding the correct properties from the index.

--- src/datastoreNoteTranslator.js
+++ src/datastoreNoteTranslator.js
@@ -11,54 +11,98 @@ class DatastoreNoteTranslator {
 
     /**
      * @param {Note} note 
-     * @param {object} Datastore document.
+     * @param {object} Datastore entity.
      */
     format(note) {
-        let key;
-
-        if (note.id !== null) {
-            // Existing notes have a key compounded with their KIND.
-            key = this.datastore.key([DatastoreNoteTranslator.DATASTORE_KIND, note.id]);
-        } else {
-            // New note shall receive a new key.
-            key = this.datastore.key(DatastoreNoteTranslator.DATASTORE_KIND);
-        }
+        const key = this._formatKey(note);
+        const data = this._formatData(note);
 
         return {
             key,
-            data: [
-                this._formatDataProperty(
-                    DatastoreNoteTranslator.DATASTORE_KIND_PROPERTIES.DATE,
-                    note.date),
-                this._formatDataProperty(
-                    DatastoreNoteTranslator.DATASTORE_KIND_PROPERTIES.TEXT,
-                    note.text)
-            ]
+            data
         }
     }
 
+    _formatKey(note) {
+        let key, idAsInt;
+
+        const noteId = note.id;
+        if (noteId === null) {
+            // Let Datastore generate the key!
+            // TODO: Consider calling allocateIds() and its usefulness in batching or creating multiple objects at once.
+            key = this.datastore.key(DatastoreNoteTranslator.DATASTORE_KIND);
+        } else {
+            try {
+                idAsInt = parseInt(noteId, 10);
+            } catch (err) {
+                console.log("Error parsing number out of ID (maybe name):", idAsInt);
+                idAsInt = noteId; // Returning as is, hoping it's in the database as a name. :s
+            }
+            key = this.datastore.key([DatastoreNoteTranslator.DATASTORE_KIND, idAsInt]);
+        }
+        
+        return key;
+    }
+
+    _formatData(note) {
+        var date = this._formatDataProperty(
+            DatastoreNoteTranslator.DATASTORE_KIND_PROPERTIES.DATE,
+            note.date);
+        var text = this._formatDataProperty(
+            DatastoreNoteTranslator.DATASTORE_KIND_PROPERTIES.TEXT,
+            note.text);
+        return [date, text];
+    }
+
     _formatDataProperty(propertyName, noteValue) {
-        const indexedProperties = DatastoreNoteTranslator.DATASTORE_KIND_INDEXED_PROPERTIES;
-        const excludeFromIndexes = (indexedProperties.indexOf(propertyName) !== -1);
+        const indexedProps = DatastoreNoteTranslator.DATASTORE_KIND_INDEXED_PROPERTIES;
         return {
             name: propertyName,
             value: noteValue,
-            excludeFromIndexes
+            excludeFromIndexes: (indexedProps.indexOf(propertyName) === -1)
         }
     }
 
     /**
-     * @param {object} datastoreDocument 
+     * @param {object} datastoreEntity 
      * @returns {Note}
      */
-    read(datastoreDocument) {
-        const meta = datastoreDocument[this.datastore.KEY];
-        const date = datastoreDocument[DatastoreNoteTranslator.DATASTORE_KIND_PROPERTIES.DATE];
-        const text = datastoreDocument[DatastoreNoteTranslator.DATASTORE_KIND_PROPERTIES.TEXT];
+    read(datastoreEntity) {
+        const id = this._readId(datastoreEntity);
+        const date = this._readProperty(datastoreEntity,
+            DatastoreNoteTranslator.DATASTORE_KIND_PROPERTIES.DATE);
+        const text = this._readProperty(datastoreEntity,
+            DatastoreNoteTranslator.DATASTORE_KIND_PROPERTIES.TEXT);
+
         return new this.Note()
-            .setId(meta.id)
+            .setId(id)
             .setDate(date)
             .setText(text);
+    }
+
+    _readId(datastoreEntity) {
+        const entityKeyPropertyName = this.datastore.KEY;
+        const meta = datastoreEntity[entityKeyPropertyName];
+        let id = meta.id;
+
+        if (typeof id === "undefined") {
+            if (typeof meta.name === "string") {
+                console.log("Datastore entity has name instead of numeric ID.",
+                    "Name:", meta.name);
+                id = meta.name; // Returning the name, so at least we can refer back to it. :s
+            } else {
+                console.log("Datastore entity doesn't have ID or name!",
+                    datastoreEntity);
+                id = null;
+            }
+        }
+
+        return id;
+    }
+
+    _readProperty(datastoreEntity, propertyName) {
+        const value = datastoreEntity[propertyName];
+        return value;
     }
 }
 DatastoreNoteTranslator.TRANSLATOR_ID = "datastore-note-translator";
